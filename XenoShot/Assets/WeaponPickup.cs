62f98c253f172a91c7b829011ec1c71aec86075f
Mo Azza <ssbsdmg@gmail.com>
2024-04-27 22:44:27 +0800

    Added randomly generated weapon spawn
    - Weapon spawns of a random rarity will now appear automatically
    - Created a generic weapon orb prefab to handle randomly generated weapon rarity setting and weapon assignment
    - Linked this system with the previously created UI weapon pickup indicator

--- XenoShot/Assets/WeaponPickup.cs
+++ XenoShot/Assets/WeaponPickup.cs
@@ -6,31 +6,38 @@ public class weaponPickup : MonoBehaviour
 {
     public RaycastWeapon weaponFab;
     public DisplayWeaponPickup pickup;
-    public string weaponName;
 
-    public bool isCommon;
-    public bool isRare;
-    public bool isEpic;
-    public bool isLegendary;
+    private string weaponName;
+    private int rarityIndex;
+    private GameObject displayModel;
+
+    public Material[] rarityMaterials;
+    public RaycastWeapon[] weaponList;
+    public GameObject[] weaponModels;
+
+    [SerializeField] private MeshRenderer mRenderer;
+
+
+    public void Start()
+    {
+        //we determine the rarity of the drop, and set rarityindex to the corresponding number
+        SetRarity();
+
+
+        //based on the rarityindex, we select the correct meterial from the array we've filled out in the inspector.
+        //the materials in the inspector should be placed in order of rarity
+        mRenderer.material = rarityMaterials[rarityIndex-1];
+
+        int weaponSelectIndex = Random.Range(0, weaponList.Length);
+
+        weaponFab = weaponList[weaponSelectIndex];
+
+        displayModel = Instantiate(weaponModels[weaponSelectIndex], gameObject.transform);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (isCommon)
-        {
-            pickup.DisplayPickupText(1, weaponName);
-        } 
-        else if (isRare)
-        {
-            pickup.DisplayPickupText(2, weaponName);
-        }
-        else if (isEpic)
-        {
-            pickup.DisplayPickupText(3, weaponName);
-        }
-        else if (isLegendary)
-        {
-            pickup.DisplayPickupText(4, weaponName);
-        }
+        pickup.DisplayPickupText(rarityIndex, displayModel.name);
 
         ActiveWeapon activeWeapon = other.gameObject.GetComponent<ActiveWeapon>();
 
@@ -39,5 +46,28 @@ public class weaponPickup : MonoBehaviour
             RaycastWeapon weapon = Instantiate(weaponFab, activeWeapon.weaponParent);
             activeWeapon.EquipWeapon(weapon);
         }
+    }
+
+    private void SetRarity()
+    {
+        int raritySelector = Random.Range(0, 100);
+
+        if(raritySelector <= 50)
+        {
+            rarityIndex = 1;
+        } 
+        else if(raritySelector > 50 && raritySelector < 70)
+        {
+            rarityIndex = 2;
+        } 
+        else if(raritySelector >= 70 && raritySelector < 90)
+        {
+            rarityIndex = 3;
+        }
+        else if(raritySelector >= 90)
+        {
+            rarityIndex = 4;
+        }
+
     }
 }
